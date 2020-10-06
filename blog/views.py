f942146d4b8c7e9fbbe572cea77ca07cc148966a
Oscar Fabra <oscarfabra@gmail.com>
2020-10-05 23:50:20 -0500

    Adds comment functionality on posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -18,12 +18,39 @@ def post_list(request):
   except EmptyPage:
     # If page is out of range deliver last page of results
     posts = paginator.page(paginator.num_pages)
-  return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+  return render(request, 
+                'blog/post/list.html', 
+                {'page': page, 
+                'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
-  post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-  return render(request, 'blog/post/detail.html', {'post': post})
+  post = get_object_or_404(Post, slug=post, 
+                                status='published', 
+                                publish__year=year, 
+                                publish__month=month, 
+                                publish__day=day)
+  # List of active comments for this post
+  comments = post.comments.filter(active=True)
+  new_comment = None
+  if request.method == 'POST':
+    # A comment was posted
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+      # Create Comment object but don't save to database yet
+      new_comment = comment_form.save(commit=False)
+      # Assign the current post to the comment
+      new_comment.post = post
+      # Save the comment to the database
+      new_comment.save()
+  else:
+    comment_form = CommentForm()
+  return render(request, 
+                'blog/post/detail.html', 
+                {'post': post, 
+                'comments': comments, 
+                'new_comment': new_comment, 
+                'comment_form': comment_form})
 
 
 class PostListView(ListView):
@@ -51,4 +78,6 @@ def post_share(request, post_id):
       sent = True
   else:
     form = EmailPostForm()
-  return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+  return render(request, 'blog/post/share.html', {'post': post, 
+                                                  'form': form, 
+                                                  'sent': sent})
