08ee3eabaff515e6fbc637bc5ac85f0c65cb8632
Oscar Fabra <oscarfabra@gmail.com>
2020-10-08 22:26:41 -0500

    Adds full-text search functionality with postgresql

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+# from django.contrib.postgres.search import TrigramSimilarity
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
@@ -102,9 +103,15 @@ def post_search(request):
     form = SearchForm(request.GET)
     if form.is_valid():
       query = form.cleaned_data['query']
+      search_vector = SearchVector('title', weight='A') + \
+                      SearchVector('body', weight='B')
+      search_query = SearchQuery(query)
       results = Post.published.annotate(
-        search=SearchVector('title', 'body'),
-      ).filter(search=query)
+        rank=SearchRank(search_vector, search_query)
+      ).filter(rank__gte=0.3).order_by('-rank')
+      # results = Post.published.annotate(
+      #   similarity=TrigramSimilarity('title', query),
+      # ).filter(similarity__gt=0.1).order_by('-similarity')
   return render(request, 'blog/post/search.html',
                           {'form': form,
                           'query': query,
